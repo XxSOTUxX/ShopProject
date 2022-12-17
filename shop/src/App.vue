aa02d9455a84963316601abbf5349969eb710e64
XxSOTUxX <anton_002233@mail.ru>
2022-12-17 19:05:42 +0300

    VueProject start

---
+++ shop/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
